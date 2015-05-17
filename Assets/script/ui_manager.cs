55d0132122e2de48fa106d0eb0020f5d5e67accd
RBEGamer <marcel.ochsendorf@gmail.com>
2015-05-17 02:06:23 +0200

    collection ant v4.1 test ant spawning

--- Assets/script/ui_manager.cs
+++ Assets/script/ui_manager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class ui_manager : MonoBehaviour {
 
 	// Use this for initialization
@@ -11,5 +11,18 @@ public class ui_manager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+
+	public GameObject slider_holder;
+
+
+
+	public void set_ressource_ants_amounts(float value){
+	//	slider_holder.GetComponent<Slider>().minValue = 0;
+	//	slider_holder.GetComponent<Slider>().maxValue = von allen ressources die max ants - alle in gebrauch
+		//get selected
+		//value nach int
+		//change value
 	}
 }
