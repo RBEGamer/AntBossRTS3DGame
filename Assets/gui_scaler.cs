53293e64c4cb357d780614996eb84ae3241509a3
RBEGamer <marcel.ochsendorf@gmail.com>
2015-06-30 16:50:29 +0200

    dynamic ui size

---
+++ Assets/gui_scaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class gui_scaler : MonoBehaviour {
+
+
+	float scale_pixel_factor = 0.15f/268.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	this.gameObject.GetComponent<CanvasScaler>().scaleFactor = Screen.width*scale_pixel_factor;
+
+
+
+
+
+
+
+
+	}
+}
