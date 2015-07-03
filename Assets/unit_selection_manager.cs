599854ecdc5d15cb4d21118e28af945a483b1984
RBEGamer <marcel.ochsendorf@gmail.com>
2015-07-04 00:07:36 +0200

    base ui v8

---
+++ Assets/unit_selection_manager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class unit_selection_manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public Sprite empty_group_holder;
+
+
+	public Sprite[] group_icons;
+	// Update is called once per frame
+	void Update () {
+	
+		for (int i = 1; i < 18; i++) {
+			GameObject.Find("unit_selection_btn_" + i.ToString()).GetComponent<Image>().sprite = empty_group_holder;
+		}
+
+		int counter =0 ;
+		foreach (UnitGroupFriendly group in GameObject.Find(vars.UnitGroupUIManager).GetComponent<UnitGroupUIManager>().unitGroupList){
+			counter++;
+			GameObject.Find("unit_selection_btn_" + counter.ToString()).GetComponent<Image>().sprite = group_icons[counter-1];
+
+		}
+
+
+	}
+}
