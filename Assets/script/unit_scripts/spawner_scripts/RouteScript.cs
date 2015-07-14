f7eedee6cf7e06b7d69d0adcdb830289405a6fe5
Kevin Heese <keksx92@googlemail.com>
2015-07-14 07:30:01 +0200

    spawn waypoints in scene view

--- Assets/script/unit_scripts/spawner_scripts/RouteScript.cs
+++ Assets/script/unit_scripts/spawner_scripts/RouteScript.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 
+[ExecuteInEditMode]  
 public class RouteScript : MonoBehaviour {
 	public List<GameObject> wayPointObjects;
-
 	public bool isOccupied = false;
+
+	//public Color routeColor;
+
+	public bool clickToShowRoute;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +18,20 @@ public class RouteScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Selection.activeGameObject == this.gameObject) {
+			if(wayPointObjects.Count > 1) {
+				float colorstep = 1.0f/(float)wayPointObjects.Count;
+
+				for(int i = 0; i < wayPointObjects.Count-1; i++) {
+					
+					Debug.DrawRay(wayPointObjects[i].transform.position,
+					              wayPointObjects[i+1].transform.position - wayPointObjects[i].transform.position, 
+					              new Color(1 - i*colorstep,1 - i*colorstep,1 - i*colorstep),
+					              2.0f);
+				}
+			}
+		}
 	}
 }
+
+
